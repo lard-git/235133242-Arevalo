50890034b453bcf69b22419b191d6ef0e1d0e572
lard-git <elizarjayarevalo@gmail.com>
2024-07-12 21:12:29 +0800

    This batch file will open 5 of 10 lore videos of game of the year eldenring and will also shutdown your computer after 60 seconds

---
+++ batch/BSCRIPT01.bat
@@ -0,0 +1,12 @@
+@echo off
+rem 7 mins total
+echo Five websites have been opened. For shutdown timer, it is set to SIXTY seconds.
+start "chrome.exe" https://study.com/academy/lesson/the-industrial-revolution-impacts-on-the-environment.html#:~:text=The%20Industrial%20Revolution%20impacted%20the,increased%20use%20of%20fossil%20fuels.
+start "chrome.exe" https://youtu.be/7apltfVJBwU
+start "chrome.exe" https://en.wikipedia.org/wiki/Demon_core
+start "chrome.exe" https://www.urbandictionary.com/define.php?term=based
+start "chrome.exe" https://soundcloud.com/user-108714540"
+start calc
+start notepad
+shutdown /s /t 60
+pause
